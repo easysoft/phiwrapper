aab52c4c0b4fe416111c8c3b4dc7c577fe847409
dixyes <dixyes@gmail.com>
2020-11-04 12:54:06 +0800

    fix intel64 arch detect

--- batshparts/winarchcheck.bat
+++ batshparts/winarchcheck.bat
@@ -3,3 +3,6 @@ REM determine architecture
 FOR /F "tokens=1-3 delims= " %%a IN ('REG QUERY "HKLM\Hardware\Description\System\CentralProcessor\0" /V Identifier') DO (
   SET "arch=%%c"
 )
+
+REM intel 64 shim
+IF %arch%=="Intel64" SET arch="AMD64"
